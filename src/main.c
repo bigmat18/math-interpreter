3609df63fe43151be38527e84acb4924e278bac3
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2023-01-18 21:38:40 +0100

    Implement file rading [to fix]

--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
-#include "tokenize.h"
+#include <stdlib.h>
+//include "tokenize.h"
 
 
 int main(int argc, char *argv[]){
-    printf("Hello lenguage!");
+    FILE* file = fopen(argv[1], 'r');
+    int size = 10, index = 0;
+    char* character = malloc(size*sizeof(char));
+
+    while(feof(file)){
+        if(index < size) character[index] = getc(file);
+        else {
+            size += 10;
+            character = realloc(character, size * sizeof(char));
+            character[index] = getc(file);
+        }
+        index++;
+    }
+    
+    size = index - 1;
+    character = realloc(character, size * sizeof(char));
+
+    for(int i = 0; i<size; i++)
+        printf("%c", character[i]);
     return 0;
 }
